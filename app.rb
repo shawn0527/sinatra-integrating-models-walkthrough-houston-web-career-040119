c37c1f829a22b37e1e1c62504d36fa7a5a73150d
Shawn Ren <shawnren527@gmail.com>
2019-04-23 04:49:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
+# require_relative 'config/environment'
+# require_relative 'models/text_analyzer.rb'
+#
+#
+# class App < Sinatra::Base
+#   get '/' do
+#     erb :index
+#   end
+#
+#   post '/' do
+#     text_from_user = params[:user_text]
+#     @analyzed_text = TextAnalyzer.new(text_from_user)
+#
+#     erb :results
+#   end
+# end
+
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
-
 
 class App < Sinatra::Base
   get '/' do
@@ -8,8 +24,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-    @analyzed_text = TextAnalyzer.new(text_from_user)
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
