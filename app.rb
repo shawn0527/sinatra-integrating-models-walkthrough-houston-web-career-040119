592001f874f1bda0a2fe1c630ad6a68359094541
Shawn Ren <shawnren527@gmail.com>
2019-04-23 04:54:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/results' do
+  post '/' do
     text_from_user = params[:user_text]
     @analyzed_text = TextAnalyzer.new(text_from_user)
     erb :results
